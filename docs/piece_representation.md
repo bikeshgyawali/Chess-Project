9547315d40b3e6f48bb155805d147eed42675700
Bikesh <bikeshgyawali1@gmail.com>
2025-12-28 19:57:07 +0000

    Creation of classes for the pieces

--- docs/piece_representation.md
+++ docs/piece_representation.md
@@ -1,3 +1,5 @@
 Date : Dec 28 2025
 
 I think representing the pieces by inheriting the color class is going to make things easier for me.
+It turns out I had to create the super() function to send back the info to Pieces class.
+Now when a user intializes a piece using a named piece constructor (like Bishop()), both a piece and bishop class are created! I dont see how that would be useful for now though.
--- src/pieces.py
+++ src/pieces.py
@@ -1,8 +1,41 @@
+#Classes for pieces
 
-
-class Color:
-    def __init__(self,color):
+class Piece():
+    def __init__(self, name, color, position):
+        self.name = name
         self.color = color
+        self.position = position
 
 
-        
+class Bishop(Piece):
+    def __init__(self, color, position):
+
+        super().__init__("Bishop", color, position)
+
+class Knight(Piece):
+    def __init__(self, color, position):
+
+        super().__init__("Knight", color, position)
+
+class Rook(Piece):
+    def __init__(self, color, position):
+
+        super().__init__("Rook", color, position)
+
+class King(Piece):
+    def __init__(self, color, position):
+
+        super().__init__("King", color, position)
+
+class Queen(Piece):
+    def __init__(self, color, position):
+
+        super().__init__("Queen", color, position)
+
+class Pawn(Piece):
+    def __init__(self, color, position):
+
+        super().__init__("Pawn", color, position)
+
+
+
