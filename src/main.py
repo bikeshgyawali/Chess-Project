6bd2db81bb6181ee811879fdbc83ac6e9540edc3
Bikesh <bikeshgyawali1@gmail.com>
2025-12-28 21:07:59 +0000

    Implemented main logic for game loop

---
+++ src/main.py
@@ -0,0 +1,24 @@
+move_list = []
+white_turn = True
+black_turn = False
+is_checkmate = False
+
+while(is_checkmate == False):
+
+    if(white_turn == True):
+        
+        white_turn_tracker = input("White to move : ")
+        white_turn = False
+        black_turn = True
+        move_list.append(white_turn_tracker)
+
+    if(black_turn == True):
+        black_turn_tracker = input("Black to move : ")
+        black_turn = False
+        white_turn = True
+        move_list.append(black_turn_tracker)
+        
+
+
+
+        
