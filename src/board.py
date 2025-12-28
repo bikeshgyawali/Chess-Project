4c4013838b547eae054460dc24022012f6a812b3
Bikesh <bikeshgyawali1@gmail.com>
2025-12-28 20:26:51 +0000

    Creation of the board

--- src/board.py
+++ src/board.py
@@ -1,4 +1,19 @@
+from pieces import Pawn, Rook, Queen, King, Bishop, Knight
+
 #Using 2d vector for chess board mapping
+
+class Board:
+    def __init__(self):
+
+        self.grid = [[0 for _ in range(8)] for _ in range(8)]
+        
+        for i in range(8):
+            self.grid[6][i] = Pawn("Black", [6, i])
+            self.grid[1][i] = Pawn("White", [1, i])
+
+
+my_chess_board = Board()
+
 
 
 
