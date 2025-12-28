5c57626187cfecc28cddba2e587bc96479c496ce
Bikesh <bikeshgyawali1@gmail.com>
2025-12-28 20:38:03 +0000

    Hardcoding pieces

--- src/board.py
+++ src/board.py
@@ -11,6 +11,27 @@ class Board:
             self.grid[6][i] = Pawn("Black", [6, i])
             self.grid[1][i] = Pawn("White", [1, i])
 
+        # White
+        self.grid[0][0] = Rook("White", [0, 0])
+        self.grid[0][7] = Rook("White", [0, 7])
+        self.grid[0][1] = Knight("White", [0, 1])
+        self.grid[0][6] = Knight("White", [0, 6])
+        self.grid[0][2] = Bishop("White", [0, 2])
+        self.grid[0][5] = Bishop("White", [0, 5])
+        self.grid[0][3] = Queen("White", [0, 3])
+        self.grid[0][4] = King("White", [0, 4])
+
+        # Black
+        self.grid[7][0] = Rook("Black", [7, 0])
+        self.grid[7][7] = Rook("Black", [7, 7])
+        self.grid[7][1] = Knight("Black", [7, 1])
+        self.grid[7][6] = Knight("Black", [7, 6])
+        self.grid[7][2] = Bishop("Black", [7, 2])
+        self.grid[7][5] = Bishop("Black", [7, 5])
+        self.grid[7][3] = Queen("Black", [7, 3])
+        self.grid[7][4] = King("Black", [7, 4])
+
+
 
 my_chess_board = Board()
 
